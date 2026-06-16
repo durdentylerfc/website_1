8a04a46de1a5c68caa27200c8eca2cc459b1a791
Patrick Hollywood <durdentylerfc923@protonmail.com>
2026-06-15 20:53:47 -0400

    add /script/main.js updated .gitignore

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/*if (navigator.storage && navigator.storage.persist) {
+  navigator.storage.persist().then((persistent) => {
+    if (persistent) {
+      console.log("Storage will not be cleared except by explicit user action");
+    } else {
+      console.log("Storage may be cleared by the UA under storage pressure.");
+    }
+  });
+}
+*/
+/*
+navigator.storage.estimate().then(estimate => {
+  console.log("Used:", estimate.usage / 1024 / 1024, "MB");
+  console.log("Quota:", estimate.quota / 1024 / 1024, "MB");
+});
+*/
+navigator.storage.estimate().then((estimate) => {
+  document.getElementById("percent").textContent = (
+    (estimate.usage / estimate.quota) * 100
+  ).toFixed(2);
+  document.getElementById("quota").textContent =
+    `${(estimate.quota / 1024 / 1024).toFixed(2)}MB`;
+});
